e8bd43960c54e13b0397de02ea4d4a959c3279b1
salexzee <samwebb@codingtestsite.com>
2016-05-03 11:00:20 -0700

    progress

--- crawlem.rb
+++ crawlem.rb
@@ -2,6 +2,7 @@ require "mechanize"
 
 class WebCrawler
   def initialize(file_name)
+    # Set the @file instance variable
     @file = file_name
   end
 
@@ -10,7 +11,9 @@ class WebCrawler
   def save_site_crawl(site_url)
     begin
       if check(site_url)
+        # open the file in append mode
         File.open(@file, "a") do |data|
+          # append the crawled information
           data.puts site_url
         end
       end
@@ -38,6 +41,39 @@ class WebCrawler
   public
 
   def crawl
+    links_found = 0
+    agent = Mechanize.new
+
+    agent.agent.http.varify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    fetched_urls = fetch_database_urls
+
+    fetched_urls.each do |url_to_crawl|
+
+      page = agent.get(url_to_crawl)
+
+      links = page.links
+
+      links.each do |link|
+        scraped_url = link.attributes['href']
+        next if scraped_url == "#"
+
+        case scraped_url[0..4]
+        when "https" then
+        when "http:" then
+        when "ftp:" then
+        else
+        end
+      end
+
+    end
   end
 
 end
+
+crawl = WebCrawler.new("./urls.txt")
+
+crawl.crawl
+
+
+
